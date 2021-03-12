164881d097179c62036625b5c47510c950cdcef6
chotiwich <chotiwich6793@gmail.com>
2021-03-12 09:56:50 +0700

    Updates DB

--- database/product.js
+++ database/product.js
@@ -1,69 +1,69 @@
 product = [
     {
-        "product_id:"01",
+        "product_id":"01",
         "product_name":"Notebook Huawei Matebook D15 R7",
         "image_path":"https://img.advice.co.th/images_nas/pic_product4/A0134460/A0134460OK_ORI_1.jpg",
         "price":"18,900",
         "detail":"HUAWEI Matebook D 15 ขับเคลื่อนด้วยโปรเซสเซอร์ AMD Ryzen 7 3700U3 ขุมพลังทรงประสิทธิภาพและความเร็วสูง ติดตั้งด้วยระบบ PCIe SSD4 พร้อมหน่วยความจำสูงถึง 8 GB แบบ Dual-Channel ภายใน DDR4 RAM ซึ่งมีกำลังการอ่านและเขียนอันรวดเร็ว ประกอบกับหน้าจอกราฟิกแบบ RadeonTM RX Vega 10 ตอบโจทย์ทั้งการทำงานและเติมเต็มอรรถรสของความบันเทิงได้เต็มที่"
     },
     {
-        "product_id:"02",
+        "product_id":"02",
         "product_name":"Notebook HP 15-eh0002AU",
         "image_path":"https://img.advice.co.th/images_nas/pic_product4/A0134460/A0134460OK_ORI_1.jpg",
         "price":"24,900",
         "detail":"หน้าจอแสดงผลขนาด 15.6 ระดับ FHD IPS หน่วยประมวลผล AMD Ryzen 7 4700U Processor หน่วยประมวลผลกราฟิก AMD Radeon Graphics (Integrated) มาพร้อม SSD PCIe NVMe ความจุ 1TB และ RAM DDR4 16GB มาพร้อมระบบปฏิบัติการ Windows 10 Home มีซอฟต์แวร์ Office Home & Student 2019 ในเครื่อง"
     },
     {
-        "product_id:"03",
+        "product_id":"03",
         "product_name":"Notebook MSI Summit E15 A11SCST-219TH",
         "image_path":"https://img.advice.co.th/images_nas/pic_product4/A0134241/A0134241OK_ORI_1.jpg",
         "price":"54,400",
         "detail":"Designed for the business elites, SUMMIT series provides best-in-class productivity, security, reliability and style. Meant for those who's constantly striving to succeed and take their business to new heights."
     },
     {
-        "product_id:"04",
+        "product_id":"04",
         "product_name":"Notebook DELL XPS 9700-W5671300THW10",
         "image_path":"https://img.advice.co.th/images_nas/pic_product4/A0132862/A0132862OK_ORI_1.jpg",
         "price":"93,390",
         "detail":"หน้าจอแสดงผลขนาด 17.0 ระดับ UHD พร้อมจอทัชสกรีน ระบบประมวลผล Intel Core i7-10750H Processor ระบบประมวลผลกราฟิก  NVDIA GeForce GTX 1650 Ti with Max-Q Design มาพร้อม SSD PCIe NVMe ความจุ 1TB และ RAM DDR4 16GB มาพร้อมระบบปฏิบัติการ Windows 10 Home"
     },
     {
-        "product_id:"05",
+        "product_id":"05",
         "product_name":"Notebook Asus D533UA-BQ001TS",
         "image_path":"https://img.advice.co.th/images_nas/pic_product4/A0135183/A0135183OK_ORI_1.jpg",
         "price":"21,900",
         "detail":"หน้าจอแสดงผลขนาด 15.6 ระดับ FHD หน่วยประมวลผล AMD Ryzen 5 5500U Processor หน่วยประมวลผลกราฟิก AMD Radeon Graphics (Integrated Graphics) มาพร้อม SSD ความจุ 512GB และ RAM DDR4 8GB มาพร้อมระบบปฏิบัติการ Windows 10 Home มีซอร์ฟแวร์ Microsoft Office Home&Student 2019"
     },
     {
-        "product_id:"06",
+        "product_id":"06",
         "product_name":"Notebook MSI GF63 Thin 9SCSR-656TH",
         "image_path":"https://img.advice.co.th/images_nas/pic_product4/A0132499/A0132499OK_ORI_1.jpg",
         "price":"26,900",
         "detail":"Latest 9th Gen Intel Core i7 processor Windows 10 Home Latest GeForce GTX 1650 Ti graphics with Max-Q design 15.6 Full HD (1920x1080), 144Hz, IPS level display Thin bezel design Aluminum hairbrush aesthetics with asymmetric pattern 21.7mm thin, 1.86kg light"
     },
     {
-        "product_id:"07",
+        "product_id":"07",
         "product_name":"Notebook Acer Nitro AN515-44-R28F/T005",
         "image_path":"https://img.advice.co.th/images_nas/pic_product4/A0133112/A0133112OK_ORI_1.jpg",
         "price":"26,900",
         "detail":"หน้าจอแสดงผลขนาด 15.6 ระดับ FHD IPS 144Hz หน่วยประมวลผล AMD Ryzen 5 4600H Processor หน่วยประมวลผลกราฟิก NVIDIA GeForce GTX 1650 Ti Graphics มาพร้อม SSD PCIe NVMe ความจุ 512GB และ RAM DDR4 8GB มาพร้อมระบบปฏิบัติการ Windows 10 Home"
     },
     {
-        "product_id:"08",
+        "product_id":"08",
         "product_name":"Notebook HP Pavilion Game 15-ec1028AX",
         "image_path":"https://img.advice.co.th/images_nas/pic_product4/A0131994/A0131994OK_ORI_1.jpg",
         "price":"29,900",
         "detail":"หน้าจอแสดงผลขนาด 15.6 ระดับ FHD IPS 144Hz หน่วยประมวลผล AMD Ryzen 7 4800H Processor หน่วยประมวลผลกราฟิก NVIDIA GeForce GTX 1660 Ti Graphics มาพร้อม SSD ความจุ 512GB และ RAM DDR4 8GB มาพร้อมระบบปฏิบัติการ Windows 10 Home",
     },
     {
-        "product_id:"09",
+        "product_id":"09",
         "product_name":"Notebook DELL Inspironl Gaming G5-W56656500THW10",
         "image_path":"https://img.advice.co.th/images_nas/pic_product4/A0133251/A0133251OK_ORI_1.jpg",
         "price":"50,900",
         "detail":"หน้าจอแสดงผลขนาด 15.6 ระดับ FHD 300Hz ระบบประมวลผล Intel Core i7-10750H Processor ระบบประมวลผลกราฟิก NVIDIA GeForce RTX 2070 with Max-Q Design มาพร้อม SSD ความจุ 1TB และ RAM DDR4 16GB มาพร้อมระบบปฏิบัติการ Windows 10 Home",
     },
     {
-        "product_id:"10",
+        "product_id":"10",
         "product_name":"Notebook Acer Predator PT515-52-73DU/T002",
         "image_path":"https://img.advice.co.th/images_nas/pic_product4/A0132439/A0132439OK_ORI_1.jpg",
         "price":"66,900",
